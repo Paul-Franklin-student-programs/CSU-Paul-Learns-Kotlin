a66ee9ba138b2711c5fe53bd51d9fd4da7443cfe
Paul-Franklin-student-programs <pfranklin138@gmail.com>
2024-12-02 18:54:35 -0500

    First test in emulator shows problem with TextView

--- app/src/main/java/MainActivity.kt
+++ app/src/main/java/MainActivity.kt
@@ -11,16 +11,37 @@ class MainActivity : AppCompatActivity() {  // Extend AppCompatActivity
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)  // Call the parent class method
         setContentView(R.layout.activity_main)  // Link to the XML layout
+
+
+        val myManager = ListManager()
+
+        val buttonForTaskAdd = findViewById<Button>(R.id.buttonForTaskAdd)
+        val textFieldForTasks = findViewById<EditText>(R.id.textFieldForTasks)
+        val markTaskDoneButton = findViewById<Button>(R.id.markTaskDoneButton)
+        val textFieldForID = findViewById<EditText>(R.id.textFieldForID)
+        val spaceForTaskList = findViewById<TextView>(R.id.spaceForTaskList)
+
+        buttonForTaskAdd.setOnClickListener {
+            val taskName = textFieldForTasks.text.toString()
+            if (taskName.isNotEmpty()) {
+                myManager.addItem(taskName)
+                spaceForTaskList.setText(myManager.showAllTasks().toString())
+            }
+        }
+
+        markTaskDoneButton.setOnClickListener {
+            val finishedTaskID = textFieldForID.text.toString().toInt()
+            if (finishedTaskID.toString().isNotEmpty() && finishedTaskID.toString()
+                    .toIntOrNull() != null
+            ) {
+                myManager.markItemAsComplete(finishedTaskID)
+                spaceForTaskList.setText(myManager.showAllTasks().toString())
+            }
+
+        }
     }
+}
 
-
-    private val myManager = ListManager()
-
-    val buttonForTaskAdd = findViewById<Button>(R.id.buttonForTaskAdd)
-    val textFieldForTasks = findViewById<EditText>(R.id.textFieldForTasks)
-    val markTaskDoneButton = findViewById<Button>(R.id.markTaskDoneButton)
-    val textFieldForID = findViewById<EditText>(R.id.textFieldForID)
-    val spaceForTaskList = findViewById<TextView>(R.id.spaceForTaskList)
 
 
 
